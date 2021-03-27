58ff25885c99c521925b0b66d662d4935b65bf9d
Cory Bennett <cbennett@netflix.com>
2021-03-27 10:05:26 -0700

    enable timers specifically?

--- services/install.sh
+++ services/install.sh
@@ -2,7 +2,15 @@
 set -ex
 sudo cp *.timer *.service /etc/systemd/system/.
 sudo systemctl daemon-reload
-for s in poold waterfall verifystate spafilter; do
-    sudo systemctl disable $s || true # clear out any previous bogus configuration
-    sudo systemctl enable $s
+for service in poold; do
+    sudo systemctl disable $service || true # clear out any previous bogus configuration
+    sudo systemctl enable $service
+done
+
+for timer in waterfall verifystate spafilter; do
+    # clear out any previous bogus configuration
+    sudo systemctl disable $timer || true
+    sudo systemctl disable $timer.timer || true
+
+    sudo systemctl enable $timer.timer
 done
