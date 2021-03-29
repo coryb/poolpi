91e28569eae79b667889cc8b329c3cbf631bf6e6
Cory Bennett <cbennett@netflix.com>
2021-03-28 22:13:51 -0700

    set the clock automatically to recover from power outages or daylight time shifts

--- cmd/verifystate/main.go
+++ cmd/verifystate/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"bytes"
 	"context"
 	"flag"
+	"fmt"
 	"log"
+	"strconv"
 	"strings"
+	"time"
 
 	"github.com/coryb/poolpi/events"
 	"github.com/coryb/poolpi/pb"
@@ -42,6 +46,7 @@ func main() {
 	fatalErr(err)
 
 	assertState(client)
+	setClock(ctx, client)
 	setSpaSpeed(ctx, client, "90%")
 }
 
@@ -63,6 +68,148 @@ func setSpaSpeed(ctx context.Context, c *events.Client, percent string) {
 		fatalErr(err)
 	}
 	log.Printf("Asserted Spa Speed: %s", percent)
+}
+
+func setClock(ctx context.Context, c *events.Client) {
+	n := time.Now().Local()
+	if n.Hour() > 22 || n.Hour() < 4 {
+		log.Printf("Skipping time check to avoid any timezone clock adjustments")
+		return
+	}
+
+	err := c.KeyMenu(ctx, "Default Menu")
+	fatalErr(err)
+	msg, err := c.KeyUntil(ctx, pb.Key_Right, "day")
+	fatalErr(err)
+
+	// display only shows "A" not "AM"
+	display := msg.Plain() + "M"
+
+	parsed, err := time.ParseInLocation("Monday: 3:04PM", display, n.Location())
+	fatalErr(err)
+
+	// Valid times are "now +/- 1m"
+	for _, offset := range []time.Duration{-time.Minute, 0, time.Minute} {
+		nowish := n.Add(offset).Format("Monday: 3:04PM")
+		if display == nowish {
+			log.Printf("Time Validated: %s", nowish)
+			return
+		}
+	}
+
+	err = c.KeyMenu(ctx, "Settings Menu")
+	fatalErr(err)
+	msg, err = c.KeySequence(ctx, []events.KeyPrompt{
+		{Key: pb.Key_Right, Expected: "Spa Heater1"},
+		{Key: pb.Key_Right, Expected: "Pool Heater1"},
+		{Key: pb.Key_Right, Expected: "VSP Speed Settings"},
+		{Key: pb.Key_Right, Expected: "Set Day and Time"},
+	}...)
+	fatalErr(err)
+
+	line2 := msg.Message[len(msg.Message)/2:]
+	line2 = bytes.TrimSpace(line2)
+
+	weekday := n.Weekday()
+
+	if parsed.Weekday() != weekday {
+		log.Printf("Clock wrong, weekday %s expected %s", parsed.Weekday(), n.Weekday())
+		for !isFocused(FocusDay, msg) {
+			msg, err = c.KeyUntil(ctx, pb.Key_Right, "Set Day and Time")
+			fatalErr(err)
+		}
+		key := pb.Key_Plus
+		if parsed.Weekday() > weekday {
+			key = pb.Key_Minus
+		}
+		for !strings.Contains(msg.Plain(), weekday.String()) {
+			msg, err = c.KeyUntil(ctx, key, "Set Day and Time")
+			fatalErr(err)
+		}
+	}
+
+	hour := n.Hour()
+	if parsed.Hour() != hour {
+		suffix := "A"
+		if hour > 11 {
+			suffix = "P"
+		}
+		h := hour % 12
+		if h == 0 {
+			h = 12
+		}
+		log.Printf("Setting Hour to %d%s", h, suffix)
+		for !isFocused(FocusHour, msg) {
+			msg, err = c.KeyUntil(ctx, pb.Key_Right, "Set Day and Time")
+			fatalErr(err)
+		}
+		key := pb.Key_Plus
+		if parsed.Hour()%12 > hour%12 {
+			key = pb.Key_Minus
+		}
+		for !isHour(h, suffix, msg) {
+			msg, err = c.KeyUntil(ctx, key, "Set Day and Time")
+			fatalErr(err)
+		}
+	}
+
+	minute := n.Minute()
+	if parsed.Minute() != minute {
+		for !isFocused(FocusMinute, msg) {
+			msg, err = c.KeyUntil(ctx, pb.Key_Right, "Set Day and Time")
+			fatalErr(err)
+		}
+		key := pb.Key_Plus
+		if parsed.Minute() > minute {
+			key = pb.Key_Minus
+		}
+		strMinute := fmt.Sprintf(":%02d", minute)
+		log.Printf("Setting Minute to %s", strMinute)
+		for !strings.Contains(msg.Plain(), strMinute) {
+			msg, err = c.KeyUntil(ctx, key, "Set Day and Time")
+			fatalErr(err)
+		}
+	}
+}
+
+func isHour(h int, suffix string, msg *pb.MessageEvent) bool {
+	plain := msg.Plain()
+	if !strings.HasPrefix(plain, "Set Day and Time") {
+		return false
+	}
+	if strings.Contains(plain, " "+strconv.Itoa(h)+":") && strings.HasSuffix(plain, suffix) {
+		return true
+	}
+	return false
+}
+
+type Focus int8
+
+const (
+	FocusDay    Focus = iota
+	FocusHour         = iota
+	FocusMinute       = iota
+)
+
+func isFocused(f Focus, msg *pb.MessageEvent) bool {
+	if !strings.HasPrefix(msg.Plain(), "Set Day and Time") {
+		return false
+	}
+	line2 := bytes.TrimSpace(msg.Message[len(msg.Message)/2:])
+	words := bytes.Fields(line2)
+	switch f {
+	case FocusDay:
+		log.Printf("Day Focused")
+		return words[0][0]&0x80 > 0
+	case FocusHour:
+		log.Printf("Hour Focused")
+		return words[1][0]&0x80 > 0
+	case FocusMinute:
+		log.Printf("Minute Focused")
+		split := bytes.Split(words[1], []byte{':'})
+		return split[1][0]&0x80 > 0
+	}
+	return false
 }
 
 func assertState(c *events.Client) {
